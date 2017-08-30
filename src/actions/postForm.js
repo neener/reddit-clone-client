7e3c465a80e035d06f7a67a33ec77cb2abc3f889
Janine Rosen <janinemrosen@gmail.com>
2017-08-30 15:22:47 -0400

    fixing typos in PostForm and adding postForm action creator

---
+++ src/actions/postForm.js
@@ -0,0 +1,7 @@
+// ** Action Creators **
+export const updatePostFormData = postFormData => {
+	return {
+		type: 'UPDATED_DATA',
+		postFormData
+	}
+}
--- src/containers/PostForm.js
+++ src/containers/PostForm.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { updatePostFormData } from '../actions/postForm';
+
 class PostForm extends Component {
 
+	handleOnChange = event => {
+		const { name, value } = event.target; 
+		const currentPostFormData = Object.assign({}, this.props.postFormData, {
+			[name]: value
+		})
+		this.props.updatePostFormData(currentPostFormData)
+	}
+
 	render() {
+		const { title, link, img_url } = this.props.postFormData;
+		
 		return (
 			<div>
 				Create a New Post
 				<form>
-				
+					<div>
+						<label htmlFor="title">Title:</label>
+						<input
+							type="text"
+							onChange={this.handleOnChange}
+							name="title"
+							value={title}
+						/>
+					</div>
+					<div>
+						<label htmlFor="link">Link:</label>
+						<input
+							type="text"
+							onChange={this.handleOnChange}
+							name="link"
+							value={link}
+						/>
+					</div>
+					<div>
+						<label htmlFor="img_url">Image Url:</label>
+						<input
+							type="text"
+							onChange={this.handleOnChange}
+							name="img_url"
+							value={img_url}
+						/>
+					</div>
 				</form>
 			</div>
 		)
@@ -17,8 +55,8 @@ class PostForm extends Component {
 
 const mapStateToProps = state => {
 	return {
-		surfboardFormData: state.surfboardFormData
+		postFormData: state.postFormData
 	}
 }
 
-export default connect(mapStateToProps)(PostForm);
+export default connect(mapStateToProps, { updatePostFormData })(PostForm);
