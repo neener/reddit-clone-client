10ee496f8028788a1815e7f0d77adac07945a333
Janine Rosen <janinemrosen@gmail.com>
2017-09-03 18:22:21 -0400

    updating postsReducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,21 +1,55 @@
+import commentsReducer from './commentsReducer';
+
 export default (state = [], action) => {
 	switch(action.type) {
-		case 'GET_POSTS_SUCCESS':
-			return action.posts;
 
-		case 'CREATE_POST_SUCCESS':
+		case 'SET_POSTS': {
+			return action.posts;
+		};
+
+		case 'ADD_POST': {
 			return state.concat(action.post);
+		};
+
+		case 'REPLACE_POST': {
+			const index = state.findIndex(post => post.id === action.post.id);
+
+			return [
+				...state.slice(0, index),
+				action.post,
+				...state.slice(index + 1)
+			];
+		};
 
 		case 'REMOVE_POST': {
-			return [
-        		...state.slice(0, action.postIndex),
-        		...state.slice(action.postIndex + 1)
-      		];
-		}
+			const index = state.findIndex(post => post.id === action.postId);
+            
+            return [
+                ...state.slice(0, index),
+                ...state.slice(index + 1)
+            ];
+		};
 
-		default:
+		case 'SET_COMMENTS':
+        case 'ADD_COMMENT':
+        case 'REPLACE_COMMENT':
+        case 'REMOVE_COMMENT': {
+            const index = state.findIndex(post => post.id === action.postId);
+            const post = state[index];
+            const updatedPost = Object.assign({}, post, {
+                comments: commentsReducer(post.comments, action)
+            });
+
+            return [
+                ...state.slice(0, index),
+                updatedPost,
+                ...state.slice(index + 1)
+            ];
+        };
+
+		default: {
 			return state;
-
-	}
-}
+		};
+	};
+};
 
